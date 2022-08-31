03c2dbfbdc382e18e1a25ada934b4cc667e2550a
Ianina Rodi <ianinarodi@gmail.com>
2022-08-31 17:55:53 -0300

    cambios para array

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let nombre = prompt ("Ingrese su nombre") ;
 
 let cursoNumero, precio, descuento, pagar;
@@ -20,3 +19,23 @@ if (cursoNumero === 1) {
 } else if (cursoNumero === 3) {
      alert ("O pagalo en 12 cuotas sin interés de $4.875");
 }
+
+const productos= ["inglés para negocios", "inglés general","inglés para viajes","inglés exámenes internacionales "];
+const precios= [2500, 2000, 2800, 3000];
+
+console.table(productos);
+console.table(precios);
+
+productos.push("inglés para conversación");
+precios.push(2300);
+
+console.table(productos);
+console.table(precios);
+
+
+for (let i=0; i<=4; i++){
+    precios[i]= precios[i] * 1.21;
+}
+
+console.table(precios);
+
