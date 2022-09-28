fa28d85d291c34973f60d303b41af425e35d5148
Ianina Rodi <ianinarodi@gmail.com>
2022-09-27 23:03:48 -0300

    optimizando

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
+
 //ARRAY DE USUARIOS REGISTRADOS
 let boton = document.getElementById('botonRegistrarme');
 boton.addEventListener("click", capturar);
 
 function capturar (){
-     function Persona (nombre, email){
-          this.nombre = nombre;
-          this.email = email;
-     }
+     function Persona (nombre, edad, email, contraseña){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.email = email;
+        this.constraseña = contraseña;       
+   }
 
-    let nombreCapturar = document.getElementById("nombre").value;
-    let emailCapturar = document.getElementById("email").value;
-    let nuevoUsuario = new Persona (nombreCapturar, emailCapturar);
-    agregar (nuevoUsuario);
+   let nombreCapturar = document.getElementById("nombre").value;
+   let edadCapturar = document.getElementById("edad").value;
+   let emailCapturar = document.getElementById("email").value;
+   let contraseñaCapturar = document.getElementById("contraseña").value;
+  let nuevoUsuario = new Persona (nombreCapturar,edadCapturar ,emailCapturar,contraseñaCapturar);
+  console.log(nuevoUsuario);
+  agregar (nuevoUsuario);
 }
-
+      
 let baseDatos = [];
 
 function agregar(nuevoUsuario) {
- console.log("capturado");
- baseDatos.push(nuevoUsuario);
- console.log(baseDatos);
- document.getElementById ("tablaUsuario").innerHTML += "<tbody><td>" + nuevoUsuario.nombre + "</td><td>" + nuevoUsuario.email + "</td><tbody>";
- const enJSON = JSON.stringify(baseDatos);
- localStorage.setItem("basedatosEJ", enJSON);
- let baseDatosEnLS = JSON.stringify(localStorage.getItem("Db"))
+  let baseDatosDeLs = JSON.parse(localStorage.getItem('basedatosEJ')) 
+  if (baseDatosDeLs == null) { 
+    baseDatosDeLs = []
+  } 
 
+    baseDatosDeLs.push(nuevoUsuario) 
+    baseDatosDeLs.forEach(usuario => {
+      document.getElementById('tablaUsuario').innerHTML +=
+        '<tbody><td>' + usuario.nombre + '</td><td>' + usuario.edad + '</td><td>' + usuario.email + '</td><tbody>' 
+    })
+    localStorage.setItem('basedatosEJ', JSON.stringify(baseDatosDeLs)) 
+//SPREAD
+console.log(...baseDatosDeLs);
 }
+
+//OPCIONES DE LIBROS SELECCIONADOS VISIBLES EN UN DIV
+const selectElement = document.getElementById('libros');
+selectElement.addEventListener('change', (event) => {
+    const resultado = document.getElementById('resultado');
+    resultado.textContent = `Seleccionaste el libro:  ${event.target.value}`;
+});
+
+// OPERADORES AVANZADOS 1
+baseDatos =JSON.stringify(localStorage.getItem("baseDatos")) || [] 
+
+
+
 
 
